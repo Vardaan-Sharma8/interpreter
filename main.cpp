8a4eeb18857dde65d09908876181d760eb7955e7
Vardaan Sharma <vardaan.bijnor@gmail.com>
2026-07-14 00:40:24 +0530

    added some interpretation

--- main.cpp
+++ main.cpp
@@ -1,18 +1,44 @@
 #include <iostream>
+#include <string>
 
-#define LOG(x) std::cout << x << std::endl;
+#define LOG(x) std::cout << x;
 
-void interpret(char& content){
+enum TokenType{
+    INT = 0, OPERATOR
+};
+
+struct Token{
+    int type;
+    char op_value;
+    int num_value;
+};
+
+int current_pos = 0;
+
+void interpret(std::string& content){
+    std::string content_ptr = content;
+    char c;
+    c = content_ptr[current_pos];
+    Token current_tokken;
     
+    if (c >= '0' && c <= '9'){
+        current_tokken.type = INT;
+        current_tokken.num_value = c - '0';
+    }
+    else{
+        current_tokken.type = OPERATOR;
+        current_tokken.op_value = c;
+    }
+
 }
 
 int main() {
 
-    char content;
+    std::string content;
 
     while(true){
-        LOG(">>>");
-        std::cin >> content;
+        LOG(">>> ");
+        std::getline(std::cin, content);
         interpret(content);
     }
 
