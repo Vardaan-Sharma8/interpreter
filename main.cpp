ea6e2b5a36d47e70a728b1118212b20e242462ff
Vardaan Sharma <vardaan.bijnor@gmail.com>
2026-07-14 20:13:33 +0530

    added token array

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 
-#define LOG(x) std::cout << x;
+#define LOG(x) std::cout << x << std::endl;
 
 //Number will have '0' token type
 //Operators will have '1' token type
@@ -19,8 +19,10 @@ struct Token{
 const std::string INT_TYPE = "Integer";
 const std::string OP_TYPE = "Operator";
 
-Token current_token;
-int current_pos = 0;
+int current_pos_str = 0;
+int current_pos_arr = 0;
+Token token_arr[1000];
+int token_num;
 
 bool is_digit(char& c){
     return (c >= '0' && c <= '9');
@@ -28,18 +30,21 @@ bool is_digit(char& c){
 
 void get_next_token(std::string content){
     char c;
-    c = content[current_pos];
+    c = content[current_pos_str];
         
     if (is_digit(c)){
-        current_token.type = INT;
-        current_token.num_value = c - '0';
-        current_token.type_str = INT_TYPE;
+        token_arr[current_pos_arr].type = INT;
+        token_arr[current_pos_arr].num_value = c - '0';
+        token_arr[current_pos_arr].op_value = '_';
+        token_arr[current_pos_arr].type_str = INT_TYPE;
     }
     else{
-        current_token.type = OPERATOR;
-        current_token.op_value = c;
-        current_token.type_str = OP_TYPE;
+        token_arr[current_pos_arr].type = OPERATOR;
+        token_arr[current_pos_arr].op_value = c;
+        token_arr[current_pos_arr].num_value = -1;
+        token_arr[current_pos_arr].type_str = OP_TYPE;
     }
+    current_pos_str++;
 }
 
 std::string get_token_type(int& type){
@@ -49,20 +54,24 @@ std::string get_token_type(int& type){
     else if(type == 1){
         return "Operator";
     }
-    return "Error";
+    else{
+        return "Error";
+    }
 }
 
 void parser(int type){
     std::string type_str = get_token_type(type);
-    if(current_token.type != type){
-        std::cout << "Syntax Error : Expected " << type_str << " instead got " << current_token.type_str;
+    if(token_arr[current_pos_arr].type != type){
+        std::cout << "Syntax Error : Expected " << type_str << " instead got " << token_arr[current_pos_arr].type_str << std::endl;
         exit(0);
     }
 }
 
 void interpret(std::string& content){
-    get_next_token(content); //expected to be integer
-    parser(INT);
+    for(int x = 0; x < token_num; x++){
+        get_next_token(content);
+        current_pos_arr++;
+    }
 }
 
 int main() {
@@ -70,8 +79,9 @@ int main() {
     std::string content;
 
     while(true){
-        LOG(">>> ");
+        std::cout << ">>> ";
         std::getline(std::cin, content);
+        token_num = content.size();
         interpret(content);
     }
 
